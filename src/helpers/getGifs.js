c71fca11078cf5f180bffac8d60b072919aedfb1
ErikCaballeroh <erikaballero.hdz@gmail.com>
2024-12-14 23:11:36 -0600

    Se agrego el Hook FetchGifs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=i8UtOr6CHT0YN0TsqhqBERxdK0qfdhJT&limit=10&q=${category}`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=i8UtOr6CHT0YN0TsqhqBERxdK0qfdhJT&limit=9&q=${category}`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map(img => ({
